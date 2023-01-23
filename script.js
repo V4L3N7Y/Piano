3b753b06c73fd18158374e92cfa35c6c59308ca0
V4L3N7Y <valimihai154@yahoo.com>
2023-01-24 00:07:49 +0200

    piano

---
+++ script.js
@@ -0,0 +1,43 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume-slider input"),
+keysCheckbox = document.querySelector(".keys-checkbox input");
+
+let allKeys = [],
+audio = new Audio("tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    },150)
+}
+
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+    console.log(key.dataset.key);
+})
+
+const pressedKey = (e) => {
+    if(allKeys.includes(e.key)) {
+    playTune(e.key);
+    }
+    console.log(e);
+}
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+
+keysCheckbox.addEventListener("click", showHideKeys);
+volumeSlider.addEventListener("input", handleVolume);
+document.addEventListener("keydown", pressedKey);
